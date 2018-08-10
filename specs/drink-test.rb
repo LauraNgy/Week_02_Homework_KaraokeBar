6fffa539330ed50289479f7a55d941c022b2728a
Laura Nagy <ngylaura@gmail.com>
2018-08-10 11:31:33 +0100

    first commit

---
+++ specs/drink-test.rb
@@ -0,0 +1,19 @@
+require("minitest/autorun")
+require("mintest/rg")
+require_relative("../drink")
+
+class DrinkTest < MiniTest::Test
+
+  def setup
+    @drink = Drink.new("Mojito", 3)
+  end
+
+  def test_drink_has_name
+    assert_equal("Mojito", @drink.name)
+  end
+
+  def test_drink_has_price
+    assert_equal(3, @drink.price)
+  end
+
+end
